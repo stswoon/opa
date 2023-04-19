0a6d965c1bdf92c99f3fafcaa5f7f087c022da28
nekhozhin <nekhozhin@netcracker.com>
2023-04-19 10:08:14 +0400

    init

---
+++ server/src/roomService.ts
@@ -0,0 +1,24 @@
+import { WS } from "./utils"
+
+//TODO: lazy_remove for ws disconnect
+//TODO: interface to work with WS
+//TODO: routers in server.ts
+//TODO: macros (e.g. title) + hashsum (e.g. for js) in index.html
+
+const LAZY_REMOVE_TIMEOUT = 10;//sec;
+
+export const createOrJoinRoom = (ws: WS, roomId: string, userId: string, userName: string): void => {
+    console.log(`User (${userName} : ${userId}) is entering in the room (${roomId})`);
+
+    ws.on("message", function (msg: string) {
+        if (msg === "H") {
+            console.log(`client send heartbit H, userId=${userId}`);
+        }
+    });
+    ws.on("close", function () {
+        console.log(`WS for user (${userName} : ${userId}) in room (${roomId}) was closed`);
+    });
+    ws.on("error", function (err: any) {
+        console.error(`WS for user (${userName} : ${userId}) in room (${roomId}) was closed`, err);
+    });
+}
---
+++ server/src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import expressWs from 'express-ws';
+import {createOrJoinRoom} from './roomService';
+import {WS} from './utils';
+import path from "path";
+
+const app = express();
+
+const unexpectedExceptionHandle = (cause: any) => console.error("Something went wrong: ", cause);
+process.on('uncaughtException', unexpectedExceptionHandle);
+process.on('unhandledRejection', unexpectedExceptionHandle);
+app.use((error: any, req: any, res: any, next: any): void => {
+    unexpectedExceptionHandle(error);
+    res.status(500).send("Server Error");
+});
+
+app.get('/health', (req, res) => res.send('OK'));
+app.use(express.static(__dirname + '/public', {extensions: ['html']}));
+app.get('/room', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+
+
+const appWs = expressWs(app);
+
+appWs.app.ws('/api/roomState', (ws: WS, req: express.Request, next): void => {
+    const {roomId, userId, userName} = req.query as { roomId: string, userId: string, userName: string };
+    console.info(`WS request roomId=${roomId}, userId=${userId} userName=${userName}`);
+    try {
+        createOrJoinRoom(ws, roomId, userId, userName);
+    } catch (error) {
+        // https://scoutapm.com/blog/express-error-handling
+        next(error); // passing to default middleware error handler
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
