04955a688e2d839fcb7a86ee9139a65574dbe187
nekhozhin <nekhozhin@netcracker.com>
2023-04-21 09:49:57 +0400

    fix deploy

--- server/src/public/roomWsRoute.ts
+++ server/src/public/roomWsRoute.ts
@@ -2,7 +2,7 @@ import {WS} from "../utils";
 import express from "express";
 import {createOrJoinRoom} from "../roomService";
 
-export const roomWsRoute = (ws: WS, req: express.Request, next): void => {
+export const roomWsRoute = (ws: WS, req: express.Request, next: any): void => {
     const {roomId, userId, userName} = req.query as { roomId: string, userId: string, userName: string };
     console.info(`WS request roomId=${roomId}, userId=${userId} userName=${userName}`);
     try {
