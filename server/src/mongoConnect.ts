effbd24bfd5f406e262b3f87a6057b872e41e3c4
nekhozhin <nekhozhin@netcracker.com>
2023-04-21 09:40:29 +0400

    fix deploy

--- server/src/mongoConnect.ts
+++ server/src/mongoConnect.ts
@@ -4,6 +4,7 @@ export const mongoConnect = async () => {
     if (!process.env.MONGO_URL) {
         throw new Error("Please add the MONGO_URL environment variable");
     }
+    console.log(`Connecting to ${process.env.MONGO_URL}`);
     try {
         await mongoose.connect(process.env.MONGO_URL);
     } catch (e) {
