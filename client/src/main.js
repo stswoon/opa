28415a153f11b12defc6d99ad9c1a238429f8dbc
nekhozhin <nekhozhin@netcracker.com>
2023-04-24 10:16:03 +0400

    choose components approach

---
+++ client/src/main.js
@@ -0,0 +1,39 @@
+// // import {LitElement, css, html} from 'lit';
+// import {LitElement, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+//
+// export class SimpleGreeting extends LitElement {
+//     static properties = {
+//         name: {},
+//     };
+//     // Define scoped styles right with your component, in plain CSS
+//     static styles = css`
+//     :host {
+//       color: blue;
+//     }
+//   `;
+//
+//     constructor() {
+//         super();
+//         // Declare reactive properties
+//         this.name = 'World';
+//     }
+//
+//     // Render the UI as a function of component state
+//     render() {
+//         return html`<p>Hello, ${this.name}!</p>`;
+//     }
+// }
+// window.customElements.define('simple-greeting', SimpleGreeting);
+
+
+
+    // import uuid from 'https://cdn.jsdelivr.net/npm/uuid@9.0.0/+esm'
+// alert(uuid());
+// import { v4 as uuidv4 } from 'https://cdn.jsdelivr.net/npm/uuid@9.0.0/+esm';
+// import {v4 as uuidv4} from 'https://cdn.jsdelivr.net/npm/uuid@9.0.0/dist/index.min.js';
+// alert(uuid());
+// import { v4 as uuidv4 } from 'uuid';
+// alert(uuidv4()); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+
+import './test.js'
